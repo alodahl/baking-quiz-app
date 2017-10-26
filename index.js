836d6c3c35b36813079f0d19bdddcca45e641914
Alina L <alodahl@pointloma.edu>
2017-10-26 10:09:22 -0700

    Changed background, refactored correct answer checking

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 "use strict";
 var $ = window.jQuery;
 
-console.log("running index.js")
-
 const questionList = [
   {questionText: "All of the following sugars are white, except:",
   answers:[
@@ -95,223 +93,129 @@ const questionList = [
   rightAnswerIndex:1
 }];
 
-
-
-
 let i = 0;
 let numCorrect = 0;
-//NB: answer indeces are compared to hard-coded correct indeces in the handleSubmit and handleStart button functions
-
-function getCorrectFeedbackPageOnSubmit(){
-        $('.insertQuizQuestion').on('click', `.js-submit-radio-form`, event => {
-          event.preventDefault();
-          renderFeedbackPage();
-        })
-}
 
 function handleStartQuizButtonClick() {
   $('body').on('click', '.js-start-quiz-button', event => {
     event.preventDefault();
-  //on button click on the start or final page, reset correct answers to 0,
-  //render quiz page 0 to DOM (aka question 1)
-  //and record only a correct radio choice to the numCorrect total.
-  
-  i = 0;
-  numCorrect = 0;
-  renderQuestionPage(i);
-  $(".start-page").addClass("hidden");
-   
-  $("input[name='answer1']").change(function(){
-      numCorrect++;
-      console.log(numCorrect);
-      getCorrectFeedbackPageOnSubmit();
-    })
-  
-  console.log("handleStartQuizButtonClick ran");
-})}
-
-function handleSubmitAnswerButtonClick() {
-    $('.insertQuizQuestion').on('click', `.js-submit-radio-form`, event => {
-      event.preventDefault();
-      
-  if (i < 9) {
-    i++;
+    //on button click on the start or final page, reset correct answers to 0,
+    //render quiz page 0 to DOM (aka question 1)
+    //and record only a correct radio choice to the numCorrect total.
+    i = 0;
+    numCorrect = 0;
     renderQuestionPage();
-  
-    if (i==1) {
-      $("input[name='answer2']").change(function(){
-      numCorrect++;
-      console.log(numCorrect);
-      getCorrectFeedbackPageOnSubmit();
-    })
-    } else if (i==2) {
-      $("input[name='answer0']").change(function(){
-      numCorrect++;
-      console.log(numCorrect);
-      getCorrectFeedbackPageOnSubmit();
-    })
-    } else if (i==3) {
-      $("input[name='answer0']").change(function(){
-      numCorrect++;
-      console.log(numCorrect);
-      getCorrectFeedbackPageOnSubmit();
-    })
-    } else if (i==4) {
-      $("input[name='answer3']").change(function(){
-      numCorrect++;
-      console.log(numCorrect);
-      getCorrectFeedbackPageOnSubmit();
-    })
-    } else if (i==5) {
-      $("input[name='answer1']").change(function(){
-      numCorrect++;
-      console.log(numCorrect);
-      getCorrectFeedbackPageOnSubmit();
-    })
-    } else if (i==6) {
-      $("input[name='answer0']").change(function(){
-      numCorrect++;
-      console.log(numCorrect);
-      getCorrectFeedbackPageOnSubmit();
-    })
-    } else if (i==7) {
-      $("input[name='answer2']").change(function(){
-      numCorrect++;
-      console.log(numCorrect);
-      getCorrectFeedbackPageOnSubmit();
-    })
-    } else if (i==8) {
-      $("input[name='answer1']").change(function(){
-      numCorrect++;
-      console.log(numCorrect);
-      getCorrectFeedbackPageOnSubmit();
-    })
-    } else if (i==9) {
-      $("input[name='answer1']").change(function(){
-      numCorrect++;
-      console.log(numCorrect);
-      getCorrectFeedbackPageOnSubmit();
-    })
-    }
-  } else {
-  renderFinalPage();
-  }
-  //and generate and display feedback page: 
-  //  renderFeedbackPage();
-  //this should apply to button on all question pages
-    console.log("handleSubmitAnswerButtonClick ran");
-})}
+    $(".start-page").addClass("hidden");
+  })
+}
 
-function handleNextQuestionButtonClick(pageNum) {
-  $('.insertQuizQuestion').on('click', '#js-next-ques-button', event => {
+function handleUserAnswerSubmission() {
+  $('.insertQuizQuestion').on('change', 'input', function(event) {
     event.preventDefault();
-  console.log("handleNextQuestionButtonClick ran");
-  //on button click, generate next quiz page 
-  i++;
-  
-  renderQuestionPage();
-  
-})}
+    let choice = event.currentTarget.value;
+    console.log(`user choice was ${choice} for question ${i + 1}/10. The correct answer was ${questionList[i].rightAnswerIndex}`);
 
-function renderQuestionPage(pageNum) {
-  console.log("renderQuestionPage ran");
-  //increase question number ++
-  //empty the ".insert..." div, then generate next question page and place it inside the div 
- 
-  $('.question').empty();
-  $('.insertQuizQuestion').html(generateQuestionPage(i));
-  //console.log(`${questionList[i].questionText}`);
-  //$('.question').text(`${questionList[i].questionText}`);
-   
+    $('.insertQuizQuestion').on('click', `.js-submit-radio-form`, function(event) {
+      event.preventDefault();
+      if (questionList[i].rightAnswerIndex == choice) {
+        numCorrect++;
+        console.log(`${numCorrect} correct answers`);
+        getCorrectFeedbackPage();
+      } else {
+        getIncorrectFeedbackPage();
+      }
+      i++;
+      console.log("handleUserAnswerSubmission ran");
+    })
+  })
 }
 
-function generateQuestionPage(pageNum) {
-  console.log("generateQuestionPage ran");
-  
-  return `<form class="quiz" name="quizquestion"><section class="quizquestion" id="quizquestion" role="quiz question">
-        <p class="question">Question ${pageNum + 1}/10: ${questionList[pageNum].questionText} (choose one)</p>
-        <span role="question"><input type="radio" name="answer0" id="" value="0">
-          <label for="">${questionList[pageNum].answers[0]}</label>
-          <br></span>
-        <span role="question"><input type="radio" name="answer1" id="" value="1">
-          <label for="">${questionList[pageNum].answers[1]}</label>
-          <br></span>
-        <span role="question"><input type="radio" name="answer2" id="" value="2">
-          <label for="ans-great-3">${questionList[pageNum].answers[2]}</label>
-          <br></span>
-        <span role="question"><input type="radio" name="answer3" id="" value="3">
-          <label for="ans-great-3">${questionList[pageNum].answers[3]}</label><br></span>
-          </section>
-        <button class="submit js-submit-radio-form" type='submit'>Submit</button></form>`;
+//on button click, generate next quiz page
+function handleNextQuestionButtonClick() {
+  $('.insertQuizQuestion').on('click', '.js-next-ques-button', event => {
+    event.preventDefault();
+    console.log("handleNextQuestionButtonClick ran");
+    if (i === 10) {
+      renderFinalPage();
+    } else {
+      renderQuestionPage();
+    }
+  })
 }
 
-function renderIncorrectFeedbackPage() {
-  console.log("renderFeedbackPage ran");
+function getCorrectFeedbackPage(){
+  let correctFeedbackHtml = `<section role="dialog" class="quiz">
+  <p>Correct!</p><img class="correctanswerimage" src="https://media.giphy.com/media/cqLxStdbAGVnW/giphy.gif" alt="Tina Fey eats a cupcake sandwich">
+  <button class="submit button js-next-ques-button" type="submit">Next Question</button>
+  </section>`;
+  $('.insertQuizQuestion').empty();
+  $('.insertQuizQuestion').html(`${correctFeedbackHtml}`);
+  // })
+}
+
+function getIncorrectFeedbackPage() {
   //empty the question html from the div, then generate next feedback page and place it inside the form element
   let correctIndex= `${questionList[i].rightAnswerIndex}`;
   $('.insertQuizQuestion').empty();
-  $('.insertQuizQuestion').html(`<section role="feedback popup page" class="quiz">
-       <p>Not quite!</p><span role="text">The correct answer was:${questionList[i].answers[correctIndex]}</span>
+  $('.insertQuizQuestion').html(`
+    <section role="feedback popup page" class="quiz">
+    <p>Not quite!</p>
+    <span role="text">The correct answer was: </span>
+    <span role="text">${questionList[i].answers[correctIndex]}</span>
+    <button class="js-next-ques-button button">Next Question</button>
     </section>`);
-
 }
 
-function renderFeedbackPage() {
-  console.log("renderFeedbackPage ran");
-  //empty the question html from the div, then generate next feedback page and place it inside the form element
-  $('.insertQuizQuestion').empty();
-  $('.insertQuizQuestion').html(generateFeedbackPage());
-  //return pageNum;
+//increase question number ++
+//empty the ".insert..." div, then generate next question page and place it inside the div
+
+function renderQuestionPage() {
+  console.log("renderQuestionPage ran");
+  $('.question').empty();
+  $('.insertQuizQuestion').html(generateQuestionPage());
 }
 
-function generateFeedbackPage() {
-  console.log("generateFeedbackPage ran");
-  
-  
-  
-  return`<section role="feedback page" class="quiz">
-       <p>Correct!</p><img class="correctanswerimage" src="https://media.giphy.com/media/cqLxStdbAGVnW/giphy.gif" alt="Tina Fey eats a cupcake sandwich">
-       <button id="js-next-ques-button" class="submit" type="submit">Next Question</button>
-    </section>`;
+function generateQuestionPage() {
+  console.log("generateQuestionPage ran");
+
+  return `<form class="quiz" name="quizquestion"><section class="quizquestion" id="radio-button-form" role="radiogroup">
+  <p class="question">Question ${i + 1}/10: ${questionList[i].questionText} (choose one)</p>
+  <span role="radio"><input type="radio" name="answer0" id="answer0" value="0">
+  <label for="answer0" name="answer0">${questionList[i].answers[0]}</label>
+  <br></span>
+  <span role="radio"><input type="radio" name="answer1" id="answer1" value="1">
+  <label for="answer1" name="answer1">${questionList[i].answers[1]}</label>
+  <br></span>
+  <span role="radio"><input type="radio" name="answer2" id="answer2" value="2">
+  <label for="answer2" name="answer2">${questionList[i].answers[2]}</label>
+  <br></span>
+  <span role="radio"><input type="radio" name="answer3" id="answer3" value="3">
+  <label for="answer3" name="answer3">${questionList[i].answers[3]}</label><br></span>
+  </section>
+  <button class="submit js-submit-radio-form button" type='submit'>Submit</button></form>`;
 }
 
 function renderFinalPage() {
   console.log("renderFinalPage ran");
-  
+
   $('.question').empty();
   $('.insertQuizQuestion').html(generateFinalPage());
 }
-  
+
 function generateFinalPage() {
   console.log("generateFinalPage ran");
-  return`<section role="final page" class="quiz">
-       <p>You scored ${numCorrect}/10!</p><img class="correctanswerimage"  src="https://i.giphy.com/media/l1J3A70hF2PUEh7pu/giphy.webp" alt="Tina Fey dips a grilled cheese into sheetcake">
-       <button class="submit js-start-quiz-button" type="submit">Try Again</button>
-     </section>`;
-}
-
-
-function handleCorrectAnswerCount() {
-  
-  //on answer submission, if answer matches correct answer index, add 1 to correct answer count
-  //if () {
-  //  numCorrect++;
-  //}
-  console.log("handleCorrectAnswerCount ran");
-}
-
-function displayNumberCorrect() {
-  
-  //this function handles displaying the number of correct answers to the text in the final page.
-  console.log("displayNumberCorrect ran");
+  return`<section role="dialog" class="quiz">
+  <p>You scored ${numCorrect}/10!</p><img class="correctanswerimage"  src="https://i.giphy.com/media/l1J3A70hF2PUEh7pu/giphy.webp" alt="Tina Fey dips a grilled cheese into sheetcake">
+  <button class="submit js-start-quiz-button button" type="submit">Try Again</button>
+  </section>`;
 }
 
 function runBakingQuiz() {
   //run event listeners
   handleStartQuizButtonClick();
-  handleSubmitAnswerButtonClick();
+  handleUserAnswerSubmission();
   handleNextQuestionButtonClick();
-  displayNumberCorrect();
+  // displayNumberCorrect();
 }
 
 $(runBakingQuiz);
